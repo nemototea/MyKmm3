b5f945bfa9eb73e7ec8dd02fe21fe9d924254154
nemototea <nemototea@gmail.com>
2021-07-29 17:15:16 +0900

    DBとかEntity周りの準備2

--- shared/src/androidMain/kotlin/com/example/mykmm3/data/DatabaseDriverFactory.kt
+++ shared/src/androidMain/kotlin/com/example/mykmm3/data/DatabaseDriverFactory.kt
@@ -1,4 +1,12 @@
 package com.example.mykmm3.data
 
-class DatabaseFactory {
+import android.content.Context
+import com.example.mykmm3.cache.AppDatabase
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
+
+actual class DatabaseDriverFactory(private val context: Context) {
+    actual fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(AppDatabase.Schema, context, "test.db")
+    }
 }
