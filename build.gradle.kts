b533948f55b84931841b251ba1580170863fd746
nemototea <nemototea@gmail.com>
2021-07-29 16:42:44 +0900

    gradleの設定

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,15 @@ buildscript {
         google()
         mavenCentral()
     }
+
+    val kotlinVersion = "1.4.0"
+    val sqlDelightVersion = "1.5.0"
+
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
     }
 }
 
