b22994e819fe427ea393ede63e6f40e804201cca
nemototea <nemototea@gmail.com>
2021-07-29 23:11:22 +0900

    Android側のViewでViewModelの初期化

--- androidApp/src/main/java/com/example/mykmm3/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/mykmm3/android/MainActivity.kt
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mykmm3.Greeting
 import android.widget.TextView
+import com.example.mykmm3.data.DatabaseDriverFactory
+import com.example.mykmm3.domain.repository.SpaceXRepository
+import com.example.mykmm3.presentation.main.MainProcessor
+import com.example.mykmm3.presentation.main.MainViewModel
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel = MainViewModel(
+        MainProcessor(
+            SpaceXRepository(
+                DatabaseDriverFactory(this)
+            )
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
